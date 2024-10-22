f409ea415456885123992f707fafff362da496ae
SarrveshNagarajan <70887969+SarrveshNagarajan@users.noreply.github.com>
2024-10-22 23:16:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 app.use(bodyparser.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://stockgro.onrender.com', 
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
